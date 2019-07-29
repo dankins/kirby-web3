d4f93e06a0aba91bac254d9275abe82f09146fe1
Dan Kinsley <dan@mgemi.com>
2019-07-29 12:53:48 -0400

    upgrade from readonly to signer web3

--- packages/core-sdk/src/ethereum/EthereumService.ts
+++ packages/core-sdk/src/ethereum/EthereumService.ts
@@ -7,36 +7,51 @@ const Web3HttpProvider = require("web3-providers-http");
 const Web3 = require("web3");
 
 export enum EthereumEvents {
-  NEW_PROVIDER = "ETHEREUM_NEW_PROVIDER",
+  NEW_WEB3_INSTANCE = "ETHEREUM_NEW_WEB3_INSTANCE",
 }
 
 export class EthereumService {
   private config: EthereumConfig;
   private ee: EventEmitter;
-  public provider: any;
   public web3: any;
+  public readonly: boolean;
 
   constructor(config: EthereumConfig, ee: EventEmitter) {
     this.config = config;
     this.ee = ee;
-    this.initializeProvider();
-    this.web3 = new Web3(this.provider);
+    this.readonly = true;
+    this.initializeWeb3();
   }
 
   public onNewWeb3(cb: (data: any) => void): void {
-    this.ee.on(EthereumEvents.NEW_PROVIDER, cb);
+    this.ee.on(EthereumEvents.NEW_WEB3_INSTANCE, cb);
   }
 
-  public initializeProvider(): void {
-    let newProvider;
+  public initializeWeb3(): void {
+    let readOnlyProvider;
     if (this.config.readOnlyNodeURI.startsWith("ws")) {
-      newProvider = new WebWsProvider(this.config.readOnlyNodeURI);
+      readOnlyProvider = new WebWsProvider(this.config.readOnlyNodeURI);
+      this.readonly = true;
     } else if (this.config.readOnlyNodeURI.startsWith("http")) {
-      newProvider = new Web3HttpProvider(this.config.readOnlyNodeURI);
+      readOnlyProvider = new Web3HttpProvider(this.config.readOnlyNodeURI);
+      this.readonly = true;
     } else {
       throw new Error("could not initialize provider");
     }
-    this.provider = newProvider;
-    this.ee.emit(EthereumEvents.NEW_PROVIDER, newProvider);
+    this.web3 = new Web3(readOnlyProvider);
+    this.ee.emit(EthereumEvents.NEW_WEB3_INSTANCE, this.web3);
+  }
+
+  public async requestSignerWeb3(): Promise<void> {
+    const win = window as any;
+    if (win.ethereum) {
+      console.log("injected ethereum: ", win.ethereum);
+      await win.ethereum.enable();
+      this.readonly = false;
+      this.web3 = new Web3(win.ethereum);
+      this.ee.emit(EthereumEvents.NEW_WEB3_INSTANCE, this.web3);
+    } else {
+      throw new Error("no injected web3 provided");
+    }
   }
 }
--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -2,14 +2,34 @@ import * as React from "react";
 
 import { Web3FrameProvider, Web3FrameContext, IWeb3FrameContext } from "@web3frame/react-sdk";
 
+async function getAccounts(ctx: IWeb3FrameContext, setAccount: (acct: string) => void) {
+  if (!ctx.web3frame.ethereum!.readonly) {
+    await (window as any).ethereum.enable();
+    const accts = await ctx.web3frame.ethereum!.web3.eth.getAccounts();
+    if (accts && accts.length > 0) {
+      setAccount(accts[0]);
+    }
+  }
+}
+
 const MyComponent = () => {
   const ctx = React.useContext<IWeb3FrameContext>(Web3FrameContext);
+
+  const [account, setAccount] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    getAccounts(ctx, setAccount);
+  }, [ctx]);
 
   console.log("ctx:", ctx);
   return (
     <div>
       <div>config: {JSON.stringify(ctx.web3frame.config)}</div>
-      <div>web3: {ctx.ethereum!.web3 ? "available" : "not available"}</div>
+      <div>web3: {ctx.web3frame.ethereum!.readonly ? "read only" : "signer available"}</div>
+      <div>account: {account}</div>
+      <div>
+        <button onClick={async () => ctx.web3frame.ethereum!.requestSignerWeb3()}>request signer web3 </button>
+      </div>
     </div>
   );
 };
