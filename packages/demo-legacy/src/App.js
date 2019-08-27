dd7c496a13c1ec204af80ee4be9c7be5c9beb6a1
Austin <austin.griffith@madwiremedia.com>
2019-08-27 17:59:49 -0600

    feat(demo-legacy): toying with legacy dapp to understand implementation

---
+++ packages/demo-legacy/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { KirbyProvider, KirbyContext, IKirbyContext, useSelector } from "@kirby-web3/parent-react";
+import { EthereumParentPlugin } from "@kirby-web3/plugin-ethereum";
+
+const config = {
+  dmz: {
+    targetOrigin: "http://localhost:3002",
+    iframeSrc: "http://localhost:3002",
+  },
+  ethereum: {
+    readOnlyNodeURI: process.env.REACT_APP_ETHEREUM_NODE,
+  },
+};
+
+const plugins = [new EthereumParentPlugin()];
+
+function App() {
+
+  const ctx = React.useContext(KirbyContext);
+  const ethereumPlugin = ctx.kirby.plugins.ethereum;
+
+  return (
+    <div className="App">
+      <KirbyProvider plugins={plugins} config={config}>
+        <div style={{position:"absolute",right:"5%",top:"3%"}}>
+          <Button onClick={async () => ethereumPlugin.requestSignerWeb3()} variant="primary" size="lg">Connect</Button>
+        </div>
+        <div style={{textAlign:"center", fontSize:28, width:"100%", marginTop:"30%"}}>
+          Demo legacy Kirby app.
+        </div>
+      </KirbyProvider>
+    </div>
+  );
+}
+
+export default App;
