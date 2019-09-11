3ab35149572ee4c6745eb6d6ae6c25ac2cafbdc5
Dan Kinsley <dan@mgemi.com>
2019-09-11 10:55:53 -0400

    fix(demo): linting

--- packages/child-app/src/App.test.tsx
+++ packages/child-app/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+afterAll(() => {
+  console.log("should do tear down now");
+});
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
